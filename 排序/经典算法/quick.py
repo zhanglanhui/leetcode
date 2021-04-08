ae848f8cb76f43c6cedfa3f628a705d5c473fc7e
zhanglanhui <zhanglanhui@xndm.tech>
2021-04-08 11:34:44 +0800

    1

--- 排序/经典算法/quick.py
+++ 排序/经典算法/quick.py
@@ -3,12 +3,11 @@ from typing import List
 import functools
 from utils import *
 
+
 # 算法步骤
 # 从数列中挑出一个元素，称为"基准"（pivot）。
 # 重新排序数列，所有比基准值小的元素摆放在基准前面，所有比基准值大的元素摆在基准后面（相同的数可以到任何一边）。在这个分区结束之后，该基准就处于数列的中间位置。这个称为分区（partition）操作。
 # 递归地（recursively）把小于基准值元素的子数列和大于基准值元素的子数列排序。
-
-
 def quickSort(alist):
     n = len(alist)
     __quickSort(alist, 0, n - 1)
@@ -20,12 +19,10 @@ def __quickSort(alist, l, r):
     # if (r - l <= 15):
     #     insertionSortHelp(alist, l, r)
     #     return
-
     if l >= r:
         return
     # p = partition(alist, l, r)
     p = partition(alist, l, r)
-
     __quickSort(alist, l, p - 1)
     __quickSort(alist, p + 1, r)
 
@@ -35,7 +32,6 @@ def partition(alist, l, r):
     pos = randint(l, r)
     alist[pos], alist[l] = alist[l], alist[pos]
     v = alist[l]
-    # v = alist[l]
     j = l
     i = l + 1
     while i <= r:
@@ -45,6 +41,7 @@ def partition(alist, l, r):
         i += 1
     alist[l], alist[j] = alist[j], alist[l]
     return j
+
 
 # 快速排序一些可以优化的点:
 # 当数列近乎有序的时，由于每次选取的都是第一个数，所以造成数列分割的极其不等，此时快排蜕化成O(n^2) 的算法， 此时只要随机选取基准点即可
