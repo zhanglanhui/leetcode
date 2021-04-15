a5cc0fb026bed5149ec51433e68eaaca0902d3f9
“zhanglanhui <zhanglanhui@xndm.tech>
2021-04-15 20:10:01 +0800

    结构整理

--- 461-hammingDistance.py
+++ 461-hammingDistance.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from typing import List
 import functools
+
 # from bitarray import bitarray
 #
 # #初始化一个有10个bit位的数组，初始值为0
@@ -13,6 +14,17 @@ import functools
 # #当bitarray中所有的元素都为1时，all()返回为True
 # if a.all():
 #     print("all bits are True.")
+
+# 两个整数之间的汉明距离指的是这两个数字对应二进制位不同的位置的数目。
+#
+# 给出两个整数 x 和 y，计算它们之间的汉明距离。
+#
+# 注意：
+# 0 ≤ x, y < 231.
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/hamming-distance
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 class Solution1:
     def hammingDistance(self, x: int, y: int) -> int:
--- 算法/动态规划/70-climbStairs.py
+++ 算法/动态规划/70-climbStairs.py
@@ -8,7 +8,7 @@ import functools
 # 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
 #
 # 注意：给定 n 是一个正整数。
-# https://leetcode-cn.com/problems/climbing-stairs/solution/pa-lou-ti-by-leetcode-solution/
+# https://leetcode-cn.com/problems/climbing-stairs/
 
 # 递归
 # 超出时间限制
