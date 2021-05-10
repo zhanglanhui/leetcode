b247cc0efd39a0105ba022a14c9921c144de91b8
zhanglanhui <zhanglanhui@xndm.tech>
2021-05-10 10:22:51 +0800

    update

--- 数据结构/二叉树/103-zigzagLevelOrder.py
+++ 数据结构/二叉树/103-zigzagLevelOrder.py
@@ -3,24 +3,21 @@ from typing import List
 import functools
 
 
-# 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+# 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
 #
-#  
-#
-# 示例：
-# 二叉树：[3,9,20,null,null,15,7],
+# 例如：
+# 给定二叉树 [3,9,20,null,null,15,7],
 #
 #     3
 #    / \
 #   9  20
 #     /  \
 #    15   7
-# 返回其层序遍历结果：
+# 返回锯齿形层序遍历如下：
 #
 # 来源：力扣（LeetCode）
-# 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+# 链接：https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal
 # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -31,16 +28,19 @@ class TreeNode:
 
 
 class Solution:
-    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
         if not root:
             return []
         ans = []
+        zigzag = False
         queue = [root]
         while queue:
             tmp = []
             ans.append([x.val for x in queue])
+            if zigzag: ans[-1] = ans[-1][::-1]
             for x in queue:
                 if x.left: tmp.append(x.left)
                 if x.right: tmp.append(x.right)
             queue = tmp
+            zigzag = ~zigzag
         return ans
--- 数据结构/二叉树/107-levelOrderBottom.py
+++ 数据结构/二叉树/107-levelOrderBottom.py
@@ -2,23 +2,25 @@
 from typing import List
 import functools
 
-
-# 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+# 给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
 #
-#  
-#
-# 示例：
-# 二叉树：[3,9,20,null,null,15,7],
+# 例如：
+# 给定二叉树 [3,9,20,null,null,15,7],
 #
 #     3
 #    / \
 #   9  20
 #     /  \
 #    15   7
-# 返回其层序遍历结果：
+# 返回其自底向上的层序遍历为：
+#
+# [
+#   [15,7],
+#   [9,20],
+#   [3]
 #
 # 来源：力扣（LeetCode）
-# 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+# 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii
 # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 
@@ -28,10 +30,8 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-
-
 class Solution:
-    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
         if not root:
             return []
         ans = []
@@ -43,4 +43,4 @@ class Solution:
                 if x.left: tmp.append(x.left)
                 if x.right: tmp.append(x.right)
             queue = tmp
-        return ans
+        return ans[::-1]
