4bcd9b79303b3f1361842b358905ec13073d9b28
zhanglanhui <zhanglanhui@xndm.tech>
2021-06-01 18:01:41 +0800

    update

--- python/算法/动态规划/64-minPathSum.py
+++ python/算法/动态规划/64-minPathSum.py
@@ -3,7 +3,6 @@ from typing import List
 import functools
 
 
-
 # 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
 #
 # 说明：每次只能向下或者向右移动一步。
@@ -19,9 +18,7 @@ import functools
 
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-
         pass
 
 
-
-print(Solution().minPathSum( [[1,3,1],[1,5,1],[4,2,1]]))
+print(Solution().minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]))
